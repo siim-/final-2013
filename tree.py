1b6738c0342ffe64a0db6c45b44349efcfba0283
Siim Kaspar Uustalu <siim@fooliummyts.eu>
2013-03-10 13:40:28 +0200

    Initial commit

---
+++ tree.py
@@ -0,0 +1,11 @@
+class node:
+    def __init__(self):
+        self.data = None
+        self.rbranch = None
+        self.lbranch = None
+        self.is_op = False
+
+class tree:
+    def __init__(self):
+        self.root = None
+    
