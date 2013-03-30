4ac9a5d7dde19890adacc63d182b4d5c1656f6e5
Siim Kaspar Uustalu <siim@fooliummyts.eu>
2013-03-30 13:33:38 +0200

    Clean up, jaotamine ning graafika lõpetamine

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@ from util import node,inorder_traverse,stack,make_operand
 from parse import parse
 import graphics as g
 """
-Avaldise teisendamine AST'ks modifitseeritud Dijkstra Shunting-Yard algoritmi abil
+Avaldise teisendamine AST'ks Dijkstra Shunting-Yard algoritmi abil
 """
 # sisse:
 # expr - avaldis millest ehitada puu
@@ -62,22 +62,67 @@ def execute(node):
              '*' : a*b,
              '/' : a/b,
              '^' : a**b }[node.data]
-def draw_ast(window,root):
-    c = g.Circle(g.Point(window.getWidth()//2,12),10)
-    t = g.Text(g.Point(window.getWidth()//2,12),root.data)
+
+#Inorder läbimine AST joonistamiseks
+def traverse_and_draw(window,element,posx,posy):
+    if element == None: return None
+    if(element.rbranch is not None):
+        if element.rbranch.is_op:
+            traverse_and_draw(window,element.lbranch,posx-40,posy+30)
+        else:
+            traverse_and_draw(window,element.lbranch,posx-20,posy+30)
+    else:
+        traverse_and_draw(window,element.lbranch,posx-20,posy+30)
+    #Joonistamine
+    draw_element(window,element.data,posx,posy)
+    if(element.rbranch is not None):
+        if element.rbranch.is_op:
+            traverse_and_draw(window,element.rbranch,posx+40,posy+30)
+        else:
+            traverse_and_draw(window,element.rbranch,posx+20,posy+30)
+    else:
+        traverse_and_draw(window,element.rbranch,posx+20,posy+30)
+    return True
+
+def draw_element(window,data,posx,posy):
+    global ON_SCREEN
+    c=g.Circle(g.Point(posx,posy),10)
     c.draw(window)
-    t.draw(window)
+    ON_SCREEN.push(c)
+    c = g.Text(g.Point(posx,posy),data)
+    c.draw(window)
+    ON_SCREEN.push(c)
+
+#Graafilise ekraani puhastamine
+def clear_screen(window):
+    while not ON_SCREEN.empty():
+        ON_SCREEN.pop().undraw()
+
+#Magasin hoidmaks ekraanil olevaid graafilisi elemente
+ON_SCREEN = stack()
 def main():
-    print("Sisesta tehe:")
-    win = g.GraphWin("Avaldise puud",400,300)
-    draw_tree = True
+    graphics_switch = input("Graafilised puud? (Y/n): ")
+    if(graphics_switch == "Y" or graphics_switch == "y"):
+        win = g.GraphWin("Avaldise puud",400,300)
+        draw_tree = True
+    else:
+        draw_tree = False
+    print("Sisesta tehe / q lõpetamiseks")
     #Peamine lõpmatu kordus
     while(True):
         expression = input(">> ")
+        if(expression == "q" or expression == "Q"):
+            break
+        if ON_SCREEN.size > 0:
+            clear_screen(win)
         root = parse(expression)
         print(execute(root))
         if draw_tree:
-            draw_ast(win,root)
+            #Joonistame juur elemendi ning seejärel rekursiivselt ülejaanud puu
+            draw_element(win,root.data,win.getWidth()//2,12)
+            traverse_and_draw(win,root.lbranch,win.getWidth()//2-50,42)
+            traverse_and_draw(win,root.rbranch,win.getWidth()//2+50,42)
         del root
-    win.close()
+    if draw_tree:
+        win.close()
 main()
